6430142876679d47fe65944815ca0e3e15446a7e
kiko <147737506+kikotc@users.noreply.github.com>
2023-12-07 23:23:17 -0500

    Added player image

--- core/src/com/chaostime2/game/MainGame.java
+++ core/src/com/chaostime2/game/MainGame.java
@@ -5,27 +5,30 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
+import com.badlogic.gdx.math.Rectangle;
+
 public class MainGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+	private SpriteBatch batch;
+	private Texture playerImg;
+	private Rectangle player;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		playerImg = new Texture("player.png");
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+		ScreenUtils.clear(0, 0, 0, 1);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(playerImg, 0, 0);
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		playerImg.dispose();
 	}
 }
--- desktop/src/com/chaostime2/game/DesktopLauncher.java
+++ desktop/src/com/chaostime2/game/DesktopLauncher.java
@@ -8,8 +8,10 @@ import com.chaostime2.game.MainGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setTitle("Chaos Time");
+		config.setWindowedMode(1920, 1080);
+		config.useVsync(true);
 		config.setForegroundFPS(60);
-		config.setTitle("ChaosTime2");
 		new Lwjgl3Application(new MainGame(), config);
 	}
 }

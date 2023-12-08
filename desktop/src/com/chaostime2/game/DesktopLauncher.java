24c71591d9a12d68c6900a491db7d8b5dc8ff93a
kiko <147737506+kikotc@users.noreply.github.com>
2023-12-07 22:53:27 -0500

    Initial commit

---
+++ desktop/src/com/chaostime2/game/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.chaostime2.game;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.chaostime2.game.MainGame;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("ChaosTime2");
+		new Lwjgl3Application(new MainGame(), config);
+	}
+}
